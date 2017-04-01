1766fc200a5cf19572e05a9a12f4e6a51454f8ec
Mateusz Woś <wos.mateusz16@gmail.com>
2017-04-01 15:22:46 +0200

    hardcoded bunch of chords

--- XDD/generator.py
+++ XDD/generator.py
@@ -2,34 +2,14 @@ from miditime.miditime import MIDITime
 
 
 class Generator(object):
-
     def __init__(self, bpm):
         self.bmp = bpm
 
-
-
-    def generate(self,file,path):
-        midi = MIDITime(self.bmp,path+file+'.mid',6,3,1)
+    def generate(self, file, path):
+        midi = MIDITime(self.bmp, path + file + '.mid', 6, 3, 1)
         notes = [
-            [0, 60, 127, 6],
-            [0.5, 50, 100, 1],
-            [0.5, 57, 100, 0.6],
-            [1.4, 50, 100, 1.9],
-            [1.4, 57, 100, 1.5],
-            [1.9, 50, 100, 2.4],
-            [1.9, 57, 100, 2.0],
-            [2.4, 50, 100, 2.9],
-            [2.4, 57, 100, 2.5],
 
-            [2.5, 56, 100, 3.5],
-            [3.5, 55, 100, 4.1],
-            [4.1, 56, 100, 4.7],
-            [4.7, 55, 100, 5.3],
-
-            [5.9, 57, 100, 6.9],
-            [7.5, 56, 100, 8.1],
-            [8.7, 57, 100, 9.3],
-            [9.6, 56, 100, 10.2]
+            # Testing chords which ill be using to create music
 
         ]
         midi.add_track(notes)
--- XDD/main.py
+++ XDD/main.py
@@ -4,14 +4,13 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('file', type=str, help='Output file name')
 parser.add_argument('pathFile', type=str, help='Location where output file will be stored')
-parser.add_argument('--bmp',type=int, choices=range(50,250) ,help='BMP value')
-parser.add_argument('--length', type=int, choices=range(5,60) ,help='Length of file in seconds')
-parser.add_argument('--octave', type=int, choices=range(1,7) ,help='Octave mostly used in output file')
+parser.add_argument('--bmp', type=int, choices=range(50, 250), help='BMP value')
+parser.add_argument('--length', type=int, choices=range(5, 60), help='Length of file in seconds')
+parser.add_argument('--octave', type=int, choices=range(1, 7), help='Octave mostly used in output file')
 
 
 def main():
     arguments = parser.parse_args()
     print(arguments.file, arguments.pathFile, arguments.bmp)
     midigenerator = Generator(arguments.bmp)
-    midigenerator.generate(arguments.file,arguments.pathFile)
-
+    midigenerator.generate(arguments.file, arguments.pathFile)
